5a2a645afe28dadf7c734a14bf7f00fa1cbe5949
abhiklaha2000 <abhiklaha2000@gmail.com>
2025-02-26 23:53:37 +0530

    mail

--- routes/nominee.js
+++ routes/nominee.js
@@ -2,8 +2,8 @@ const express =  require('express');
 const nominee_router = new express.Router();
 
 const bodyParser = require('body-parser');
-nominee_router.use(bodyParser.json())
-nominee_router.use(bodyParser.urlencoded({extended:true}));
+nominee_router.use(bodyParser.json({ limit: "500mb" }))
+nominee_router.use(bodyParser.urlencoded({ limit: "500mb", extended:true}));
 
 // Register Controller
 const nomineeController = require('../controllers/nominee');
